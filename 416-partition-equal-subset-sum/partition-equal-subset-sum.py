e0536136474e836e0fe1d79d3aedd0aae9bb734c
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2025-11-20 23:08:26 +0530

    Time: 1946 ms (18.74%) | Memory: 34 MB (32.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,22 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        def subsetSum(ind, target, dp):
-            if(ind == len(nums) - 1):
-                return nums[ind] == target
-            if(target == 0):
-                return True
-            if dp[ind][target] != -1:
-                return dp[ind][target]
-            notPick = subsetSum(ind + 1, target, dp)
-            pick = False
-            if nums[ind] <= target:
-                pick = subsetSum(ind + 1, target - nums[ind], dp)
-            dp[ind][target] = pick or notPick
-            return dp[ind][target]
         totalSum = sum(nums)
-        if totalSum%2 or len(nums) < 2:
+        if totalSum % 2:
             return False
         target = totalSum // 2
-        dp = [[-1 for _ in range(target+1)] for _ in range(len(nums))]
-        subsetSum(0, target, dp)
-        return dp[0][target]
+        dp = [[False for _ in range(target + 1)] for _ in range(len(nums))]
+        for i in range(len(nums)):
+            dp[i][0] = True
+        if(nums[0]<=target):
+            dp[0][nums[0]] = True
+        for i in range(1, len(nums)):
+            for j in range(1, target+1):
+                notPick = dp[i-1][j]
+                pick = False
+                if nums[i] <= j:
+                    pick = dp[i-1][j - nums[i]]
+                dp[i][j] = pick or notPick
+        return dp[len(nums)-1][target]
