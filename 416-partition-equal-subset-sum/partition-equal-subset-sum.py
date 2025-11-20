b43fa44fa150852d69f0dcfde56769841bd09577
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2025-11-20 23:28:06 +0530

    Time: 1112 ms (37.49%) | Memory: 17.9 MB (89.22%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,16 +4,21 @@ class Solution:
         if totalSum % 2:
             return False
         target = totalSum // 2
-        dp = [[False for _ in range(target + 1)] for _ in range(len(nums))]
-        for i in range(len(nums)):
-            dp[i][0] = True
+        #dp = [[False for _ in range(target + 1)] for _ in range(len(nums))]
+        prev = [False] * (target + 1)
+        
+        prev[0] = True
+        
         if(nums[0]<=target):
-            dp[0][nums[0]] = True
+            prev[nums[0]] = True
         for i in range(1, len(nums)):
+            cur = [False] * (target + 1)
+            cur[0] = True
             for j in range(1, target+1):
-                notPick = dp[i-1][j]
+                notPick = prev[j]
                 pick = False
                 if nums[i] <= j:
-                    pick = dp[i-1][j - nums[i]]
-                dp[i][j] = pick or notPick
-        return dp[len(nums)-1][target]
+                    pick = prev[j - nums[i]]
+                cur[j] = pick or notPick
+            prev = cur
+        return prev[target]
