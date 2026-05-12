a4f266b42ecbbbad4edecc4e3e08c86aea821fdf
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-05-12 16:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.05%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,16 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def backtrack(ind, dp):
-            # ====base cases====
-            if(ind == 0):
-                return nums[0]
-            if(ind == 1):
-                return max(nums[0], nums[1])
-            if(dp[ind]!=-1):
-                return dp[ind]
-            pick = nums[ind] + backtrack(ind - 2, dp)
-            notPick = backtrack(ind-1, dp)
+        lengthArr = len(nums)
+        if(lengthArr == 1):
+            return nums[0]
+        if(lengthArr == 2):
+            return max(nums[0], nums[1])
+        dp = [0 for _ in range(lengthArr)]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for ind in range(2, lengthArr):
+            #convert reccurence into iteration
+            pick = nums[ind] + dp[ind-2]
+            notPick = dp[ind-1]
             dp[ind] = max(pick, notPick)
-            return dp[ind]
-        dp = [-1 for _ in range(len(nums))]
-        return backtrack(len(nums)-1, dp)
+        return dp[lengthArr-1]
+        
