d44fc52d9a50968b9358a879973a09e6d882ce1f
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-05-13 21:06:47 +0530

    Time: 1744 ms (16.33%) | Memory: 19.8 MB (74.63%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,13 @@
 from typing import List
 
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
-        for i in range(len(nums)):
-            val = target - nums[i]
-            if val in hashMap:
-                return [hashMap[val], i]
-            hashMap[nums[i]] = i
-        return [-1, -1]
+        n = len(nums)
+        # Iterate through each element as the first addend
+        for i in range(n):
+            # Iterate through the rest of the elements as the second addend
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
