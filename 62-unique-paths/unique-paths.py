bbd0545e43241e1fb3b7e3ba32fecfb51d304886
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-05-21 22:12:30 +0530

    Time: 3 ms (20.04%) | Memory: 19.3 MB (41.88%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def f(i, j):
-            if(i == 0 & j == 0):
-                return 1
-            if(i < 0 | j < 0):
-                return 0
-            if(dp[i][j] != -1):
-                return dp[i][j]
-            up = 0
-            left = 0
-            if(i>0):up = f(i-1, j)
-            if(j>0):left = f(i, j-1)
-            dp[i][j] = up + left
-            return dp[i][j]
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return f(m-1, n-1)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if(i == 0 & j == 0):
+                    dp[i][j] = 1
+                    continue
+                up = 0
+                left = 0
+                if(i>0):up = dp[i-1][j]
+                if(j>0):left = dp[i][j-1]
+                dp[i][j] = up + left
+
+        return dp[m-1][n-1]
