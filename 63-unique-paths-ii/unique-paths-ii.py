7d58a6545f91820bc30fd0cf266c0e7f3182f075
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-05-22 00:26:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (32.84%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -2,19 +2,23 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        prev = [0 for _ in range(n)]
+        cur = [0 for _ in range(n)]
+        
         for i in range(m):
             for j in range(n):
                 if(obstacleGrid[i][j] == 1):
-                    dp[i][j] = 0
+                    cur[j] = 0
                     continue
-                if(i == 0 and j == 0) and (obstacleGrid[i][j]!=1):
-                    dp[0][0] = 1
+                if(i == 0 and j == 0):
+                    cur[0] = 1
                     continue
                
-                up = dp[i-1][j]
-                left = dp[i][j-1]
-                dp[i][j] = up + left
-    
-        return dp[m-1][n-1]
+                up = prev[j]
+                left = 0
+                if(j>0):
+                    left = cur[j-1]
+                cur[j] = up + left
+            prev = cur
+        return prev[n-1]
             
