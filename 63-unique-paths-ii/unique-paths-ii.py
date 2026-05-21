e201e4d7d764046cf0fa040ec9fa1519bbf15515
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-05-22 00:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (89.67%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        def f(i, j):
-            if(i == 0 and j == 0) and (obstacleGrid[i][j]!=1):
-                return 1
-            if(i < 0 or j < 0 or obstacleGrid[i][j] == 1):
-                return 0
-            if(dp[i][j] != -1):
-                return dp[i][j]
-            up = 0
-            left = 0
-            if(i>0):up = f(i-1, j)
-            if(j>0):left = f(i, j-1)
-            dp[i][j] = up + left
-            return dp[i][j]
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return f(m-1, n-1)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if(obstacleGrid[i][j] == 1):
+                    dp[i][j] = 0
+                    continue
+                if(i == 0 and j == 0) and (obstacleGrid[i][j]!=1):
+                    dp[0][0] = 1
+                    continue
+               
+                up = dp[i-1][j]
+                left = dp[i][j-1]
+                dp[i][j] = up + left
+    
+        return dp[m-1][n-1]
             
