0a211c382210beadfc26df8dad0b034f7b68d080
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-05-22 12:15:40 +0530

    Time: 19 ms (26.47%) | Memory: 20.4 MB (90.15%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,21 +1,21 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        def f(i, j, dp): #f(m-1, n-1)
-            if(i == 0 and j == 0):
-                return grid[0][0]
-            if(i < 0 or j < 0):
-                return float('inf')
-            if(dp[i][j] != -1):
-                return dp[i][j]
-            up = float('inf')
-            left = float('inf')
-            if(i>0):
-                up = f(i-1, j, dp)
-            if(j>0):
-                left = f(i, j-1, dp)
-            dp[i][j] = grid[i][j] + min(up, left)
-            return dp[i][j]
         m = len(grid)
         n = len(grid[0])
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return f(m-1, n-1, dp)
+        prev = [-1 for _ in range(n)] 
+        cur = [-1 for _ in range(n)] 
+        for i in range(m):
+            for j in range(n):
+                if(i == 0 and j == 0):
+                    cur[j] = grid[i][j]
+                    continue
+                up = float('inf')
+                left = float('inf')
+                if(i>0):
+                    up = prev[j]
+                if(j>0):
+                    left = cur[j-1]
+                cur[j] = grid[i][j] + min(up, left)
+            prev = cur
+
+        return prev[n-1]
