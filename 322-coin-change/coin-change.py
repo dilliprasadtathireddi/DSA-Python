c7a4603136eec41febeaa2a189afd2f0106cdcfb
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-01 19:34:51 +0530

    Time: 1863 ms (5.00%) | Memory: 23.6 MB (22.05%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,22 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def helper(i, amount):
-            if(amount == 0):
-                return 0
-            if(i == 0):
-                if(amount % coins[0] == 0):
-                    return amount // coins[0]
-                return float('inf')
-            if(dp[i][amount]!= -1):
-                return dp[i][amount]
-            notPick = helper(i-1, amount)
-            pick = float('inf')
-            if(coins[i] <= amount):
-                temp = helper(i, amount - coins[i])
-                if(temp != float('inf')):
-                    pick = 1 + temp
-            dp[i][amount] = min(notPick, pick)
-            return dp[i][amount]
-        dp = [[-1 for _ in range(amount+1)] for _ in range(len(coins))]
-        ans = helper(len(coins) - 1, amount) 
+        dp = [[float('inf') for _ in range(amount+1)] for _ in range(len(coins))]
+        for k in range(len(coins)):
+            dp[k][0] = 0
+        for i in range(1, amount+1):
+            if(i % coins[0] == 0):
+                dp[0][i] = i // coins[0]
+        
+        for i in range(1, len(coins)):
+            for j in range(1, amount+1):
+                notPick = dp[i-1][j]
+                pick = float('inf')
+                if(coins[i] <= j):
+                    temp = dp[i][j - coins[i]]
+                    if(temp != float('inf')):
+                        pick = 1 + temp
+                dp[i][j] = min(notPick, pick)
+        ans = dp[len(coins) - 1][amount]
         return ans if(ans != float('inf')) else -1
