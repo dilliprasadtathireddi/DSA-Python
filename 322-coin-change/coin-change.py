701c617113f7d1860624aaaaddc129311b470fb9
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-01 19:40:35 +0530

    Time: 1417 ms (5.27%) | Memory: 20 MB (34.50%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,20 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[float('inf') for _ in range(amount+1)] for _ in range(len(coins))]
-        for k in range(len(coins)):
-            dp[k][0] = 0
-        for i in range(1, amount+1):
+        # dp = [[float('inf') for _ in range(amount+1)] for _ in range(len(coins))]
+        if(amount == 0):return 0
+        prev = [float('inf') for _ in range(amount+1)]
+        cur = [float('inf') for _ in range(amount+1)]
+        # for k in range(len(coins)):
+        #    dp[k][0] = 0
+        for i in range(amount+1):
             if(i % coins[0] == 0):
-                dp[0][i] = i // coins[0]
+                prev[i] = i // coins[0]
         
         for i in range(1, len(coins)):
             for j in range(1, amount+1):
-                notPick = dp[i-1][j]
+                cur[0] = 0
+                notPick = prev[j]
                 pick = float('inf')
                 if(coins[i] <= j):
-                    temp = dp[i][j - coins[i]]
+                    temp = cur[j - coins[i]]
                     if(temp != float('inf')):
                         pick = 1 + temp
-                dp[i][j] = min(notPick, pick)
-        ans = dp[len(coins) - 1][amount]
+                cur[j] = min(notPick, pick)
+            prev = cur
+        ans = prev[amount]
         return ans if(ans != float('inf')) else -1
