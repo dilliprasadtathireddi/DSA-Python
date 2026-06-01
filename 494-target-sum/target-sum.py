936d761544dfc520961310cd037ddcf8569b66d6
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-01 11:45:59 +0530

    Time: 44 ms (77.19%) | Memory: 19.4 MB (59.39%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -4,24 +4,27 @@ class Solution:
         # divide into +, and - and take difference of two subsets// 
         # addittion of + subset and - subset which gives target value
         #s1 - s2 = target; x | s- x ; 2x - s = d; x = s + d / 2 or (s-d)/2
-        def helper(ind, target):
+        def helper(ind, target, dp):
             if(ind == 0):
                 if(target == 0 and nums[0] == 0):
                     return 2
                 if(target == 0 or nums[0] == target):
                     return 1
                 return 0
-            notPick = helper(ind - 1, target)
+            if(dp[ind][target] != -1):
+                return dp[ind][target]
+            notPick = helper(ind - 1, target, dp)
             pick = 0
             if( nums[ind] <= target):
-                pick = helper(ind - 1, target - nums[ind])
-            return notPick + pick
+                pick = helper(ind - 1, target - nums[ind], dp)
+            dp[ind][target] =  notPick + pick
+            return dp[ind][target]
         s = sum(nums)
         n = len(nums)
         
         if(s < target) or (s- target) % 2 :
             return 0
         neededTarget = (s - target) // 2
-        
+        dp = [[-1 for _ in range(neededTarget+1)] for _ in range(n)]
 
-        return helper(n-1, neededTarget)
+        return helper(n-1, neededTarget, dp)
