488f0556d426925ef9211713d63ee5cab5081b6e
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-09 12:51:51 +0530

    Time: 1133 ms (29.20%) | Memory: 19.5 MB (89.83%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -4,11 +4,14 @@ class Solution:
         #find lcs of s and rev
         m = len(s)
         n = len(rev)
-        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        # dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        prev = [0 for _ in range(n+1)]
         for i in range(1, m+1):
+            cur  = [0 for _ in range(n+1)]
             for j in range(1, n+1):
                 if(s[i-1] == rev[j-1]):
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    cur[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[m][n]
+                    cur[j] = max(prev[j], cur[j-1])
+            prev = cur
+        return prev[n]
