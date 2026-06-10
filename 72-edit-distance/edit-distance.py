0b1d710db53aa080049f181f1c96a73e198835da
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-11 00:25:12 +0530

    Time: 67 ms (10.59%) | Memory: 22.9 MB (29.53%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,18 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        
-        @cache
-        def helper(i ,j):
-            if(j == -1):
-                return i + 1
-            if(i == -1):
-                return j + 1
-            if(word1[i] == word2[j]):
-                return helper(i - 1, j - 1)
-            replace =  1 + helper(i - 1, j - 1)
-            insert = 1 + helper(i, j-1)
-            delete = 1 + helper(i - 1, j)
-            return min(replace, min(insert, delete))
         m = len(word1)
         n = len(word2)
-        return helper(m-1, n-1)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = i
+        for j in range(n+1):
+            dp[0][j] = j
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if(word1[i-1] == word2[j-1]):
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    replace = 1 + dp[i-1][j-1]
+                    insert = 1 + dp[i][j-1]
+                    delete = 1 + dp[i-1][j]
+                    dp[i][j] = min(replace, min(insert, delete))
+            
+        return dp[m][n]
