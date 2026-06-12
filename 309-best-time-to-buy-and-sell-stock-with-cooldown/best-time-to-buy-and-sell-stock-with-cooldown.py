edf123ca55f74397e6216d717476c5782642c5ad
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-12 12:37:16 +0530

    Time: 3 ms (61.72%) | Memory: 19.4 MB (82.75%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,16 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[0 for _ in range(2)] for _ in range(n+2)]
+        # dp = [[0 for _ in range(2)] for _ in range(n+2)]
+        front2 = [0 for _ in range(2)]
+        front = [0 for _ in range(2)]
+        cur = [0 for _ in range(2)]
         for i in range(n-1, -1, -1):
             for buy in range(2):
                 if(buy):
-                    pick = -prices[i] + dp[i+1][0]
-                    notPick = dp[i+1][1]
-                    dp[i][buy] = max(pick, notPick)
+                    pick = -prices[i] + front[0]
+                    notPick = front[1]
+                    cur[buy] = max(pick, notPick)
                 else:
                     
-                    sell = prices[i] + dp[i+2][1]
-                    notSell = dp[i+1][0]
-                    dp[i][buy] = max(sell, notSell)
-        return dp[0][1]
+                    sell = prices[i] + front2[1]
+                    notSell = front[0]
+                    cur[buy] = max(sell, notSell)
+            front2 = front[:]
+            front = cur[:]
+        return front[1]
