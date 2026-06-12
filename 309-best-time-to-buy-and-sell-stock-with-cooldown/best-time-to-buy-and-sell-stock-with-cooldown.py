9c709ddac56def335c535c0c1b8e2ab8c5210336
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-12 12:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (69.14%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,8 +4,9 @@ class Solution:
         # dp = [[0 for _ in range(2)] for _ in range(n+2)]
         front2 = [0 for _ in range(2)]
         front = [0 for _ in range(2)]
-        cur = [0 for _ in range(2)]
+        
         for i in range(n-1, -1, -1):
+            cur = [0 for _ in range(2)]
             for buy in range(2):
                 if(buy):
                     pick = -prices[i] + front[0]
@@ -16,6 +17,6 @@ class Solution:
                     sell = prices[i] + front2[1]
                     notSell = front[0]
                     cur[buy] = max(sell, notSell)
-            front2 = front[:]
-            front = cur[:]
+            front2 = front
+            front = cur
         return front[1]
