903e61477902d8115ca28c0c7083b537dcad42fc
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-12 10:21:28 +0530

    Time: 347 ms (28.85%) | Memory: 30.9 MB (72.25%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,16 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+        # dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+        front = [[0 for _ in range(3)] for _ in range(2)]
+        cur = [[0 for _ in range(3)] for _ in range(2)]
         for i in range(n-1, -1, -1):
             for buy in range(2):
                 for transactions in range(1, 3):
                     if(buy):
-                        take = -prices[i] + dp[i+1][0][transactions]
-                        notBuy = dp[i+1][1][transactions]
-                        dp[i][buy][transactions]= max(take, notBuy)
+                        take = -prices[i] + front[0][transactions]
+                        notBuy = front[1][transactions]
+                        cur[buy][transactions]= max(take, notBuy)
                     else:
-                        sell = prices[i] + dp[i+1][1][transactions - 1]
-                        notSell = dp[i+1][0][transactions]
-                        dp[i][buy][transactions]= max(sell, notSell)
-        return dp[0][1][2]
+                        sell = prices[i] + front[1][transactions - 1]
+                        notSell = front[0][transactions]
+                        cur[buy][transactions]= max(sell, notSell)
+            front = cur
+        return front[1][2]
