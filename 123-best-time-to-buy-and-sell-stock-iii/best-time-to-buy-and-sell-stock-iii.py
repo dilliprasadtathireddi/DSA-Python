a13af1a0113264a5242fce0243ae0c3424c57255
DILLI PRASAD TATHIREDDI <129867063+dilliprasadtathireddi@users.noreply.github.com>
2026-06-12 10:17:55 +0530

    Time: 798 ms (18.73%) | Memory: 62.5 MB (20.28%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,23 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def helper(i, buy, transactions):
-            if(transactions == 0):
-                return 0
-            if(i == len(prices)):
-                return 0
-            if(dp[i][buy][transactions] != -1):
-                return dp[i][buy][transactions]
-            if(buy):
-                take = -prices[i] + helper(i+1, 0, transactions)
-                notBuy = helper(i+1, 1, transactions)
-                dp[i][buy][transactions]= max(take, notBuy)
-                return dp[i][buy][transactions]
-            else:
-                sell = prices[i] + helper(i+1, 1, transactions - 1)
-                notSell = helper(i+1, 0, transactions)
-                dp[i][buy][transactions]= max(sell, notSell)
-                return dp[i][buy][transactions]
         n = len(prices)
-        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
-        
-        return helper(0, 1, 2)
+        dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+        for i in range(n-1, -1, -1):
+            for buy in range(2):
+                for transactions in range(1, 3):
+                    if(buy):
+                        take = -prices[i] + dp[i+1][0][transactions]
+                        notBuy = dp[i+1][1][transactions]
+                        dp[i][buy][transactions]= max(take, notBuy)
+                    else:
+                        sell = prices[i] + dp[i+1][1][transactions - 1]
+                        notSell = dp[i+1][0][transactions]
+                        dp[i][buy][transactions]= max(sell, notSell)
+        return dp[0][1][2]
